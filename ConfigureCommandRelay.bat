678ee8fcc552f9fe9a3162f856ac15b8533566ca
Mithras Kuipers <mithraskuipers@gmail.com>
2026-08-28 13:02:49 +0200

    update

--- ConfigureCommandRelay.bat
+++ ConfigureCommandRelay.bat
@@ -1,2 +1,3 @@
 @echo off
-powershell -STA -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%~dp0ConfigGUI.ps1"
+powershell -NoProfile -NoLogo -Command "Unblock-File -Path '%~dp0ConfigGUI.ps1'" >nul 2>&1
+powershell -STA -NoProfile -NoLogo -ExecutionPolicy Bypass -Scope Process -File "%~dp0ConfigGUI.ps1"
--- StartCommandRelay.bat
+++ StartCommandRelay.bat
@@ -1,3 +1,4 @@
 @echo off
-powershell -STA -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%~dp0CommandRelay.ps1"
+powershell -NoProfile -NoLogo -Command "Unblock-File -Path '%~dp0CommandRelay.ps1'" >nul 2>&1
+powershell -STA -NoProfile -NoLogo -ExecutionPolicy Bypass -Scope Process -File "%~dp0CommandRelay.ps1"
 pause
